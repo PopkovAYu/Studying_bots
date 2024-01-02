bd256ddab58b2142b7132d3734bba7286cf31faf
Alexander Popkov <alexander.yu.popkov@gmail.com>
2024-01-02 22:39:36 +0700

    First bot

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-print('Just some text')
+import requests
+import time
 
-print('Some local changes')
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '6063146164:AAFCcptkgbTB_6NhKphqLMccucsJwQvFqkQ'
+TEXT = 'Cool content!'
+MAX_COUNTER = 100
 
-print('Some more changes')
+offset = -2
+counter = 0
+chat_id: int
 
-print('Again?')
+while counter < MAX_COUNTER:
+    print('attempt = ', counter)
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
