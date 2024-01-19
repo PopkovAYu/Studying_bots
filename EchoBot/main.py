8c788b6d13afcfa0b57b4e4306820134480ca0a8
Alexander Popkov <alexander.yu.popkov@gmail.com>
2024-01-19 12:35:44 +0700

    Added photo handler

--- EchoBot/main.py
+++ EchoBot/main.py
@@ -24,13 +24,18 @@ async def process_help_command(message: Message):
         'Write something and I send you your message'
     )
 
-# Handler for any other messages
-async def send_echo(message: Message):
-    await message.reply(text=message.text)
+# Handler for any other text messages
+async def send_text_echo(message: Message):
+    await message.reply(message.text)
+
+# Handler for photos
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
 
 # Handler registration
 dp.message.register(process_start_command, Command(commands='start'))
 dp.message.register(process_help_command, Command(commands='help'))
-dp.message.register(send_echo)
+dp.message.register(send_text_echo, F.content_type == ContentType.TEXT)
+dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
 if __name__ == '__main__':
     dp.run_polling(bot)
