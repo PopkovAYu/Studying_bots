7dae070f426a00092d9e0199d92852b6331015e5
Alexander Popkov <alexander.yu.popkov@gmail.com>
2024-01-19 12:56:01 +0700

    Added sticer handler and register shortened

--- EchoBot/main.py
+++ EchoBot/main.py
@@ -24,18 +24,24 @@ async def process_help_command(message: Message):
         'Write something and I send you your message'
     )
 
-# Handler for any other text messages
-async def send_text_echo(message: Message):
-    await message.reply(message.text)
-
 # Handler for photos
 async def send_photo_echo(message: Message):
     await message.reply_photo(message.photo[0].file_id)
 
+# Handler for stickers
+async def send_sticker_echo(message: Message):
+    await message.reply_sticker(message.sticker.file_id)
+
+# Handler for any other text messages
+async def send_echo(message: Message):
+    await message.reply(message.text)
+
 # Handler registration
 dp.message.register(process_start_command, Command(commands='start'))
 dp.message.register(process_help_command, Command(commands='help'))
-dp.message.register(send_text_echo, F.content_type == ContentType.TEXT)
-dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
+dp.message.register(send_photo_echo, F.photo)
+dp.message.register(send_sticker_echo, F.sticker)
+dp.message.register(send_echo)
+
 if __name__ == '__main__':
     dp.run_polling(bot)
