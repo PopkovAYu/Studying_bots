d13a55151827bbdfeea80b41da499c3d73d2dc49
Alexander Popkov <alexander.yu.popkov@gmail.com>
2024-01-19 12:19:57 +0700

    Added handler registration to echobot

--- EchoBot/main.py
+++ EchoBot/main.py
@@ -1,6 +1,6 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 from dotenv import load_dotenv
 import os
 
@@ -13,23 +13,24 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 # Handler for /start command
-@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer(
         'Hey! I am EchoBot! Write something'
     )
 
 # Handler for /help command
-@dp.message(Command(commands=["help"]))
 async def process_help_command(message: Message):
     await message.answer(
         'Write something and I send you your message'
     )
 
 # Handler for any other messages
-@dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
+# Handler registration
+dp.message.register(process_start_command, Command(commands='start'))
+dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(send_echo)
 if __name__ == '__main__':
     dp.run_polling(bot)
