033aff82db1a54fd372e104e8077b9aca50902fa
Alexander Popkov <alexander.yu.popkov@gmail.com>
2024-01-25 12:12:06 +0700

    Added bot with /start command handling without aiogram and ability to check if there is only /start and nothing else

---
+++ CommandProcessingBot/main.py
@@ -0,0 +1,24 @@
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+BOT_TOKEN = os.getenv('TOKEN')
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+def my_start_filter(message: Message) -> bool:
+    return message.text == '/start'
+
+# Same but with lambda:
+# start_check = lambda msg: msg.text == '/start'
+
+# Handler for start command
+@dp.message(start_check)
+async def process_start_command(message: Message):
+    await message.answer(text='This is /start command')
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
