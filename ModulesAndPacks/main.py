181845ddcbc315243a6a21d56c5f0f6b885ed3a9
Alexander Popkov <alexander.yu.popkov@gmail.com>
2024-01-29 18:13:53 +0700

    Added dir() to main.py

--- ModulesAndPacks/main.py
+++ ModulesAndPacks/main.py
@@ -1,11 +1,16 @@
+print(dir())
+
 import functions
 from data import my_dict
 from classes import MyClass
 
 print('This is executed file')
 
+new_variable: int = 15
+
 if __name__ == "__main__":
     print('The code below wont be executed if this file will be an import module to another executed file')
     print(functions.get_double_number(100))
     print(my_dict)
     MyClass()
+    print(dir())
